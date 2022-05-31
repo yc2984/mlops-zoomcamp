bc20de090494f8de8cc816867fc88c649595fce0
Yang <yangcao@yang-20220516.c.ewx-tryout.internal>
2022-05-31 12:32:43 +0000

    week 2 homework excluding artifacts

---
+++ week_2/homework/train.py
@@ -0,0 +1,59 @@
+import argparse
+import os
+import pickle
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+MLFLOW_TRACKING_URI = "http://127.0.0.1:5000/"
+exp_name = "nyc-taxi-homework"
+
+
+# START an experiment
+import mlflow
+mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+mlflow.set_experiment(exp_name)
+
+
+def fetch_logged_data(run_id):
+    client = mlflow.tracking.MlflowClient()
+    data = client.get_run(run_id).data
+    tags = {k: v for k, v in data.tags.items() if not k.startswith("mlflow.")}
+    artifacts = [f.path for f in client.list_artifacts(run_id, "model")]
+    return data.params, data.metrics, tags, artifacts
+
+
+def run(data_path):
+    mlflow.sklearn.autolog()
+    with mlflow.start_run() as run:
+        
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
+
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+    params, metrics, tags, artifacts = fetch_logged_data(run.info.run_id)
+    print(params)
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        default="./output",
+        help="the location where the processed NYC taxi trip data was saved."
+    )
+    args = parser.parse_args()
+
+    run(args.data_path)
